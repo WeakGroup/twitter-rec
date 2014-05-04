5dffb3ffb86dafeebd90b018bc16bc96fe677491
yisheng <liaoyisheng89@gmail.com>
2014-05-04 18:16:34 -0400

    Add last user as seed if there is a checkpoint

--- twitter_rec/crawler.py
+++ twitter_rec/crawler.py
@@ -6,28 +6,31 @@ import os
 import pickle
 import threading
 import Queue
+from collections import OrderedDict
 
 from .Api import Session
 
 class Container(object):
     def __init__(self, path):
         self.path = path
-        self.max_incr = 100 
+        self.max_incr = 10
         self.incr_count = 0
-        self.data = {}
+        self._data = OrderedDict()
+        self._last_user = None
         if os.path.exists(self.path):
           logger.D('Data already exists, load from %s', self.path)
           with open(self.path) as f:
-            self.data = pickle.load(f)
-            logger.D('Load %d celebrities', len(self.data))
+            self._data, self._last_user = pickle.load(f)
+            logger.D('Load %d celebrities', len(self._data))
         else:
           logger.D('Set data path to %s', self.path)
 
     def add(self, user):
-      if user['user_id'] in self.data:
+      if user['user_id'] in self._data:
         return
 
-      self.data[user['user_id']] = user 
+      self._data[user['user_id']] = user 
+      self._last_user = user
       self.incr_count += 1
       logger.D('Now container gets %d users totally.', self.incr_count)
 
@@ -37,10 +40,15 @@ class Container(object):
     def _flush(self):
       with open(self.path, 'w') as f:
         logger.D('Flush to checkpoint file %s.', self.path)
-        pickle.dump(self.data, f, protocol = -1)
-
+        pickle.dump((self._data, self._last_user), f, protocol = -1)
+    
     def __len__(self):
-      return len(self.data)
+      return len(self._data)
+
+    @property
+    def last_user(self):
+      return self._last_user
+    
 
 
 _lock = threading.Lock()
@@ -81,7 +89,7 @@ class Crawler(object):
         self.celebrity = Container(checkpoint_path)
         self._user_queue = Queue.Queue() 
         self._task_queue = Queue.Queue() 
-        self._workers = [] #[Crawler.Worker(username_or_email, password, self._task_queue, self._user_queue, self.celebrity)] * 10
+        self._workers = [] 
 
         for i in range(n_threads):
           self._workers.append(Crawler.Worker(username_or_email, password, self._task_queue, self._user_queue, self.celebrity))
@@ -91,7 +99,10 @@ class Crawler(object):
 
 
     def crawl(self):
-        user_seed = self.session.get_user(user_id = "liao_eason") 
+        user_seed = self.celebrity.last_user
+        if user_seed is None:
+          logger.D('There is no reserved user in container, Initialize with liao_easion')
+          user_seed = self.session.get_user(user_id = "liao_eason") 
         self._user_queue.put(user_seed)
 
         while True:
