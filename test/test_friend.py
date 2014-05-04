2779cd340df3345b0c3d96606d6127b94a55ff10
yisheng <liaoyisheng89@gmail.com>
2014-05-04 16:55:08 -0400

    restore test_friends

--- test/test_friend.py
+++ test/test_friend.py
@@ -8,12 +8,6 @@ PASSWD = "bigdata"
 s = Api.Session(USERNAME, PASSWD, debug=False)
 s.connect()
 
-
-info = s.read("/")
-print info
-
-"""
 users = s.get_friends()
 for user in users:
     print user['user_name'], user['user_id']
-"""
